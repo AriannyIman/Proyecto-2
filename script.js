33d1f36389124edc80847c9fbb04a0b867b3a117
AriannyIman <99633454+AriannyIman@users.noreply.github.com>
2022-02-21 00:34:42 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function contactus(){
+    ModalContactus = document.querySelector("#modal-contactanos");
+    ModalContactus.className = "show_modal";
+   }
+   
+   
+   
+function closecontactus(){
+    ModalContactus = document.querySelector("#modal-contactanos");
+    ModalContactus.className = "closemodal";
+}
